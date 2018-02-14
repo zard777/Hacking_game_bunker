e0dcda32d87e4c93ec514d94e39e04d3bc366726
Eric D <docomo102030@gmail.com>
2018-02-15 00:35:47 +0700

    Add files via upload

---
+++ VictorVran/_armor/Z_Adventurer.lua
@@ -0,0 +1,5 @@
+Outfit_Adventurer.modifiers_0.armor = 9900
+Outfit_Adventurer.modifiers_0.destiny_slots = 5
+Outfit_Adventurer.modifiers_0.overdrive = 800
+Outfit_Adventurer.modifiers_0.max_overdrive_add = 6000
+Outfit_Adventurer.modifiers_0.overdrive_decay_rate = 7
---
+++ VictorVran/_armor/Z_Wanderer.lua
@@ -0,0 +1,2 @@
+Outfit_Wanderer.modifiers_0.armor = 5000
+Outfit_Wanderer.modifiers_0.overdrive_regen_rate = 500
